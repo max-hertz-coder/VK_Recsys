5f43878cb27b6b16f8e44c1fb4706ee8bbfdd15e
VladimirKalajcidi <vovchik2006kalaychidi@gmail.com>
2024-11-01 20:59:40 +0300

    clf testing

--- main.py
+++ main.py
@@ -1,36 +1,16 @@
-from scipy.sparse import csr_matrix
-import polars as pl
-import implicit
-
-train = pl.scan_parquet("../data/train_interactions.parquet")
-
-train = train.filter((pl.col("like") + pl.col("dislike")) >= 1)
-train = train.with_columns(weight=pl.col("like") - pl.col("dislike"))
-train = train.select("user_id", "item_id", "weight")
+import turicreate as tc
 
 
-items_meta = pl.read_parquet("../data/items_meta.parquet")
-users_meta = pl.read_parquet("../data/users_meta.parquet")
-n_items = items_meta["item_id"].max() + 1
-n_users = users_meta["user_id"].max() + 1
+actions = tc.SFrame.read_csv('data/train_interactions.csv')
 
+users = tc.SFrame.read_csv('data/users_meta.csv')
+items = tc.SFrame.read_csv('data/items_meta.csv')
+test = tc.SFrame.read_csv('data/test_pairs.csv')
+actions['weight'] = actions['like'] - actions['dislike']
+actions = actions['user_id', 'item_id', 'weight']
 
-train = csr_matrix((train["weight"],
-                    (train["user_id"].to_numpy(),
-                     train["item_id"].to_numpy())),
-                   shape=(n_users, n_items))
+model = tc.recommneder.ranking_factorization_recommender.create(actions, 'user_id', 'item_id', target='weight', user_info=users, item_info=items)
 
-
-model = implicit.als.AlternatingLeastSquares(factors=50,
-                                             iterations=10,
-                                             regularization=1,
-                                             alpha=100,
-                                             calculate_training_loss=True)
-model.fit(train)
-
-
-test_pairs = pl.read_csv('../data/test_pairs.csv')
-
-
-als_predict = (model.user_factors[test_pairs['user_id']] *
-               model.item_factors[test_pairs['item_id']]).sum(axis=1)
+scores = model.predict(test)
+test['predict'] = scores
+test.export.csv('sub.csv')
